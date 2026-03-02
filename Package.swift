bfa93089c3614c0009e0136597a17c122d317333
Tom Everton <tom.everton@permutive.com>
2026-03-02 11:55:19 +0000

    Release v2.5.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name:"Permutive_iOS",
-		url:"https://storage.googleapis.com/permutive-ios-sdks/swift-sdk/Permutive_iOS-v2.4.1.zip",
-		checksum:"6c3dd8289fa4f407fe1a520fd88ab75b9f887edcd0ba06873a5aa9557b43605a")
+		url:"https://storage.googleapis.com/permutive-ios-sdks/swift-sdk/Permutive_iOS-v2.5.0.zip",
+		checksum:"8d9599cbaa8aaff41175d5c78c25515750682cfafdced3239145c341411398d3")
     ]
 )
